f98fde0327352530cd1e75c4c662cda269e7beea
Weerapat <weerapatsu@gmail.com>
2019-08-19 18:01:50 +0700

    Add localization

---
+++ pages/_lang/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <logo />
+        <h1 class="title">
+          motor-form-nuxt
+        </h1>
+        <h2 class="subtitle">
+          New motor form
+        </h2>
+        <div class="links">
+          <a
+            href="lead-form"
+            class="button--green"
+          >
+            Go to new motor lead form
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/_lang/lead-form.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <a href="/">Go back to homepage</a>
+        <h1>Hello new lead form</h1>
+        <form>
+          <div class="form-group">
+            <label for="email">{{ $t('lead_form.email') }}</label>
+            <input type="email" class="form-control" id="email" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <label for="car-model">Select list:</label>
+            <select class="form-control" id="car-model" v-model="user.carModel">
+              <option value="" selected>Please select</option>
+              <option value="honda">Honda</option>
+              <option value="toyota">Toyota</option>
+              <option value="bmw">BMW</option>
+              <option value="mazda">Mazda</option>
+            </select>
+          </div>
+
+          <div class="form-group" v-show="user.carModel">
+            <label for="desc">Please add detail about your car : {{ user.carModel }}</label>
+            <input type="text" class="form-control" id="desc" placeholder="Please enter">
+          </div>
+
+          <div class="form-group">
+            <label class="radio-inline"><input type="radio" name="optradio" checked>Option 1</label>
+            <label class="radio-inline"><input type="radio" name="optradio">Option 2</label>
+            <label class="radio-inline"><input type="radio" name="optradio">Option 3</label>
+            <p class="help-block">Example block-level help text here.</p>
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Check me out
+            </label>
+          </div>
+          <button type="submit" class="btn btn-success">Submit</button>
+
+          <a href="/package-listing" class="btn btn-danger">Start Compare!!</a>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        carModel: '',
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  h1 {
+    margin-top: 50px;
+  }
+</style>
